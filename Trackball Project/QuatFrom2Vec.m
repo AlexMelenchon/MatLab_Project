cd7d999b98464a8d229541cbf26bd50ebbf735f3
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2020-01-04 19:09:04 +0100

    Commented QuatFrom2Vec function

--- Trackball Project/QuatFrom2Vec.m
+++ Trackball Project/QuatFrom2Vec.m
@@ -3,18 +3,25 @@ function [q] = QuatFrom2Vec(vec1,vec2)
 % Computes the quaternion given two vectors
 % Inputs:
 %    vec1: first vector
-%    vec2: secong vector
+%    vec2: second vector
 % Outputs:
 %    q: Result quaternion
 
-
+%We firts normalize both imput vectors
 vec1=vec1/sqrt((vec1(1)^2)+(vec1(2)^2)+(vec1(3)^2));
 vec2=vec2/sqrt((vec2(1)^2)+(vec2(2)^2)+(vec2(3)^2));
 
+%We then make the cross product to find a new vector w taht is
+%perpendicular to both imput vectors
 w=cross(vec1,vec2);
+
+%We asign the vector w to the complex part of the quaternion, we do it
+%this way instead of in one line because matlab gives error
 a=w(1);
 b=w(2);
 c=w(3);
+%We asign 1 + the dot product of the imput vectors to the real part of the
+%quaternion, which gives us the rotation of the quaternion
 d=1+dot(vec1,vec2);
 q=[d;a;b;c];
 end
